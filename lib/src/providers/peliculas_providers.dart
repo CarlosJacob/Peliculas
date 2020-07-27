f51c9d3be576aa9fe529e30901390e5c36bbbdc2
Carlos Carrasco Rodriguez <carlos.carrasco@gmail.com>
2020-07-27 18:40:55 -0400

    infinitive scroll ready

--- lib/src/providers/peliculas_providers.dart
+++ lib/src/providers/peliculas_providers.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -8,6 +9,23 @@ class PeliculasProvider {
   String _apiKey = DotEnv().env['movieApiKey'];
   String _url = DotEnv().env['movieUrl'];
   String _language = 'es-ES';
+
+  int _popularesPage = 0;
+
+  List<Pelicula> _populares = new List();
+
+  final _popularesStreamController =
+      StreamController<List<Pelicula>>.broadcast();
+
+  Function(List<Pelicula>) get popularesSink =>
+      _popularesStreamController.sink.add;
+
+  Stream<List<Pelicula>> get popularesStream =>
+      _popularesStreamController.stream;
+
+  void dispouseStreams() {
+    _popularesStreamController?.close();
+  }
 
   Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
     final resp = await http.get(url);
@@ -28,10 +46,17 @@ class PeliculasProvider {
   }
 
   Future<List<Pelicula>> getPopulares() async {
+    _popularesPage++;
     final url = Uri.https(_url, '3/movie/popular', {
       'api_key': _apiKey,
       'language': _language,
+      'page': _popularesPage.toString(),
     });
-    return await _procesarRespuesta(url);
+
+    final resp = await _procesarRespuesta(url);
+
+    _populares.addAll(resp);
+    popularesSink(_populares);
+    return resp;
   }
 }
